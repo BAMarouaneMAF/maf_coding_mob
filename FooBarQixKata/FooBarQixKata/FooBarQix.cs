d5f4f677343adc21ee71a89d6c7e0d1f5309b576
BAMarouaneMAF <marouane.ben.amara@maf.fr>
2016-09-22 14:38:58 +0200

    FooBarQix Kata

---
+++ FooBarQixKata/FooBarQixKata/FooBarQix.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FooBarQixKata
+{
+    public class FooBarQix
+    {
+        public string Convert(int value)
+        {
+            if (value % 3 == 0 && value % 5 == 0)
+            {
+                return "Foo,Bar";
+            }
+            if (value % 3 == 0)
+            {
+                return "Foo";
+            }
+            if (value % 5 == 0)
+            {
+                return "Bar";
+            }
+            throw new ArgumentException();
+        }
+    }
+}
---
+++ FooBarQixKata/FooBarQixKata/FooBarQixTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Xunit;
+
+namespace FooBarQixKata
+{
+    public class FooBarQixTests
+    {
+        [Fact]
+        public void Should_Foo_When_3()
+        {
+            //Arrange
+            var fooBarQix = new FooBarQix();
+            
+            //Act
+            var actual = fooBarQix.Convert(3);
+
+            //Assert
+            Assert.Equal("Foo", actual);
+        }
+
+        [Fact]
+        public void Should_Bar_When_5()
+        {
+            //Arrange
+            var fooBarQix = new FooBarQix();
+
+            //Act
+            var actual = fooBarQix.Convert(5);
+
+            //Assert
+            Assert.Equal("Bar", actual);
+        }
+
+        [Fact]
+        public void Should_Foo_When_9()
+        {
+            //Arrange
+            var fooBarQix = new FooBarQix();
+
+            //Act
+            var actual = fooBarQix.Convert(9);
+
+            //Assert
+            Assert.Equal("Foo", actual);
+        }
+
+        [Fact]
+        public void Should_Bar_When_10()
+        {
+            //Arrange
+            var fooBarQix = new FooBarQix();
+
+            //Act
+            var actual = fooBarQix.Convert(10);
+
+            //Assert
+            Assert.Equal("Bar", actual);
+        }
+
+        [Fact]
+        public void Should_Raise_Exception_When_7()
+        {
+            //Arrange
+            var fooBarQix = new FooBarQix();
+            
+            //Assert
+            Assert.Throws<ArgumentException>(() => fooBarQix.Convert(7));
+        }
+
+        [Fact]
+        public void Should_Foo_Bar_When_15()
+        {
+            //Arrange
+            var fooBarQix = new FooBarQix();
+
+            //Act
+            var actual = fooBarQix.Convert(15);
+
+            //Assert
+            Assert.Equal("Foo,Bar", actual);
+        }
+
+        [Fact]
+        public void Should_Foo_Bar_When_30()
+        {
+            //Arrange
+            var fooBarQix = new FooBarQix();
+
+            //Act
+            var actual = fooBarQix.Convert(30);
+
+            //Assert
+            Assert.Equal("Foo,Bar", actual);
+        }
+    }
+}
